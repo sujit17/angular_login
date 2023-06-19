3c50acf3711aa3345248f650492acc240ffe3a63
Sujit Mandal <msujit017@gmail.com>
2023-06-19 10:45:49 +0530

    protected routes  if user is not login.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,10 @@
+import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   if (!localStorage.getItem('userLogin')) {
-    route.root;
+    const route: Router = inject(Router);
+    route.navigate(['/']);
     return false;
   }
   return true;
